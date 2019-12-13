6eee0c48aeda11fdb74e6216744e5f6d9480634f
LuongDzai <vuxuanluong12345@gmail.com>
2019-12-13 15:53:24 +0700

    Chia components cac thu

--- App.js
+++ App.js
@@ -1,49 +1,29 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity
-} from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/header";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: "Lượng", id: "1" },
-    { name: "Huong", id: "2" },
-    { name: "Linh", id: "3" },
-    { name: "Tien", id: "4" },
-    { name: "Kien", id: "5" },
-    { name: "C Quynh", id: "6" },
-    { name: "C Nhung", id: "7" }
+  const [todos, setTodos] = useState([
+    { text: "buy coffe", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" }
   ]);
 
-  const pressHandler = id => {
-    console.log(id);
-    setPeople((prevPeople)=>{
-      return prevPeople.filter(person => person.id != id)
-    });
-  };
   return (
     <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        keyExtractor={item => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
-      {/* <ScrollView>
-        {people.map(item => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
+      {/* header */}
+      <Header />
+      <View style={styles.content}>
+        {/* to form*/}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -51,18 +31,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20
-    // alignItems: "center",
-    // justifyContent: "center"
+    backgroundColor: "#fff"
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 30
   }
 });
