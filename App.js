51aec2299ab10c0f731548329cacd404790bae0b
LuongDzai <vuxuanluong12345@gmail.com>
2019-12-13 14:25:18 +0700

    Update phat

--- App.js
+++ App.js
@@ -2,17 +2,35 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState(
-    "Open up App.js to start working on your app!"
-  );
+  const [name, setName] = useState("Shawn");
+  const [person, setPerson] = useState({ name: "mario", age: 40 });
+
+  const clickHandler = () => {
+    setName("Vu Xuan Luong");
+    setPerson({ name: "Luongvx", age: 30 });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Change Text"
-        onPress={() => setOutputText("The text has been changed")}
-      />
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update state" onPress={clickHandler} />
+      </View>
     </View>
+
+    // <View style={styles.container}>
+    //   <View style={styles.header}>
+    //     <Text style={styles.boldText}>Hello, World</Text>
+    //   </View>
+    //   <View style={styles.body}>
+    //     <Text style={styles.boldText}>Thajat <Text>khong the tin</Text>abcxyz</Text>
+    //     <Text>abcxyz</Text>
+    //     <Text>abcxyz</Text>
+    //   </View>
+    // </View>
   );
 }
 
@@ -22,6 +40,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  buttonContainer: {
+    marginTop: 20
   }
+  // header: {
+  //   backgroundColor: "pink",
+  //   padding: 20,
+  // },
+  // boldText: {
+  //   fontWeight: "bold",
+  // },
+  // body: {
+  //   backgroundColor:'yellow',
+  //   padding: 20,
+  // }
 });
-
