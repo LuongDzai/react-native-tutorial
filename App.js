bb269ef0c0eeabe8d3f7c816f8e69c2ebe4e366b
LuongDzai <vuxuanluong12345@gmail.com>
2019-12-13 14:40:37 +0700

    Update phan state view blabla

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("Shawn");
-  const [person, setPerson] = useState({ name: "mario", age: 40 });
-
-  const clickHandler = () => {
-    setName("Vu Xuan Luong");
-    setPerson({ name: "Luongvx", age: 30 });
-  };
+  const [age, setAge] = useState("30");
 
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
       <Text>
-        His name is {person.name} and his age is {person.age}
+        Enter Name:
       </Text>
-      <View style={styles.buttonContainer}>
-        <Button title="Update state" onPress={clickHandler} />
-      </View>
+      <TextInput style={styles.input}
+      multiline
+      placeholder='e.g Luong Vu Xuan'
+      onChangeText={(val)=>setName(val)}
+      />
+      
+      <Text>
+        Enter Age:
+      </Text>
+      <TextInput style={styles.input}
+      placeholder='e.g 20'
+      keyboardType='numeric'
+      onChangeText={(val)=>setAge(val)}
+      />
+      <Text>
+        name is {name} and age is {age}
+      </Text>
     </View>
-
-    // <View style={styles.container}>
-    //   <View style={styles.header}>
-    //     <Text style={styles.boldText}>Hello, World</Text>
-    //   </View>
-    //   <View style={styles.body}>
-    //     <Text style={styles.boldText}>Thajat <Text>khong the tin</Text>abcxyz</Text>
-    //     <Text>abcxyz</Text>
-    //     <Text>abcxyz</Text>
-    //   </View>
-    // </View>
   );
 }
 
@@ -41,18 +38,34 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  buttonContainer: {
-    marginTop: 20
+  input:{
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   }
-  // header: {
-  //   backgroundColor: "pink",
-  //   padding: 20,
-  // },
-  // boldText: {
-  //   fontWeight: "bold",
-  // },
-  // body: {
-  //   backgroundColor:'yellow',
-  //   padding: 20,
-  // }
 });
+
+// <View style={styles.container}>
+//   <View style={styles.header}>
+//     <Text style={styles.boldText}>Hello, World</Text>
+//   </View>
+//   <View style={styles.body}>
+//     <Text style={styles.boldText}>Thajat <Text>khong the tin</Text>abcxyz</Text>
+//     <Text>abcxyz</Text>
+//     <Text>abcxyz</Text>
+//   </View>
+// </View>
+
+// header: {
+//   backgroundColor: "pink",
+//   padding: 20,
+// },
+// boldText: {
+//   fontWeight: "bold",
+// },
+// body: {
+//   backgroundColor:'yellow',
+//   padding: 20,
+// }
